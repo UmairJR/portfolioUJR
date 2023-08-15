1f85a9f075a84c409861628b77ad2a6812b8bcf2
Vishnu <vishnu.bagde@pivotroots.com>
2023-08-15 21:26:29 +0530

    line

--- components/ExpCard.jsx
+++ components/ExpCard.jsx
@@ -5,33 +5,31 @@ import { fadeIn, slideIn } from '@/motion'
 
 const ExpCard = (props) => {
     return (
-        <div>
+        <li className={`flex flex--align-items-center mb--25`}>
 
             <motion.div
                 variants={fadeIn('up', 'spring', props.index * 0.5, 1)}
                 className='exp--card flex'
             >
-                <li className='flex flex--align-items-center mb--25'>
-                    <div className='ml--25'>
-                        <div className='exp--title--section width--column-three-1'>
-                            <h2 className='exp--text pd--5 ml--10'>{props.title}</h2>
-                        </div>
-                        <div className='exp--company width--column-two-1 font--center'>
-                            <a href={props.company_link} className='exp--handle--text' target="_blank">
-                                {props.company}
-                            </a>
-                        </div>
-                        <div className='exp--desc width--column-two-1'>
-                            <p className='padding--left'>
-                                {props.desc}
-                            </p>
-                        </div>
+                <div className='ml--25'>
+                    <div className=''>
+                        <h2 className='exp--text'>{props.title}</h2>
                     </div>
-                </li>
+                    <div className='exp--company'>
+                        <a href={props.company_link} className='exp--handle--text' target="_blank">
+                            {props.company}
+                        </a>
+                    </div>
+                    <div className='exp--desc width--column-two-1'>
+                        <p>
+                            {props.desc}
+                        </p>
+                    </div>
+                </div>
             </motion.div>
 
 
-        </div>
+        </li>
     )
 }
 
