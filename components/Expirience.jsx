7573a8df43ad9a16526431a7bff65797db576ed2
Vishnu <vishnu.bagde@pivotroots.com>
2023-08-15 19:50:17 +0530

    experience added

--- components/Expirience.jsx
+++ components/Expirience.jsx
@@ -11,17 +11,34 @@ const Expirience = () => {
         variants={homeContainerVariant(1)}
         initial='hidden'
         whileInView='show'
-        >Work Expirience.</motion.h1>
-    <div className='flex'>
-    <div className='width--column-two-1'>
-        
-        
-        
-    </div>
-    <div className='width--column-two-1 font--center'>
-        
-    </div>
-    </div>
+        >Work Expirience.
+    </motion.h1>
+    <ul className="experience--list">
+    {
+        exp.map((exps, index) => (
+            <li className='flex flex--align-items-center mb--25'>
+                <div className='ml--25'>
+                    <div>
+                        {exps.title}
+                    </div>
+                    <div>
+                        {exps.title}
+                    </div>
+                    <div>
+                        {exps.title}
+                    </div>
+                    <div>
+                        {exps.title}
+                    </div>
+                    <div>
+                        {exps.title}
+                    </div>
+                    
+                </div>
+            </li>
+        ))
+    }
+    </ul>
 </div>
   )
 }
@@ -30,21 +47,21 @@ export default Expirience
 
 const exp = [
     {
-        'title':'SDE Intern',
-        'company':'Octaware Technologies',
-        'company-link':'https://octaware.com',
-        'certificate':'',
-        'start-date':'Jun 2022',
-        'end-date':'July 2022',
-        'desc':'Hands-on experience of MS Power Apps.Worked with a team and developed Vendor Management System.Deeply learned the phases of SDLC.'
+        title:'SDE Intern',
+        company:'Octaware Technologies',
+        company_link:'https://octaware.com',
+        certificate:'',
+        start_date:'Jun 2022',
+        end_date:'July 2022',
+        desc:'Hands-on experience of MS Power Apps.Worked with a team and developed Vendor Management System.Deeply learned the phases of SDLC.'
     },
     {
-        'title':'Web Developer Intern',
-        'company':'Insys Technologies',
-        'company-link':'https://www.insystechnologies.in',
-        'certificate':'',
-        'start-date':'Jun 2020',
-        'end-date':'Aug 2020',
-        'desc':'Developed Hospital Management System.Primarily learned and implemented – HTML, JS, PHP, CSS, and AJAX'
+        title:'Web Developer Intern',
+        company:'Insys Technologies',
+        company_link:'https://www.insystechnologies.in',
+        certificate:'',
+        start_date:'Jun 2020',
+        end_date:'Aug 2020',
+        desc:'Developed Hospital Management System.Primarily learned and implemented – HTML, JS, PHP, CSS, and AJAX'
     }
 ]
